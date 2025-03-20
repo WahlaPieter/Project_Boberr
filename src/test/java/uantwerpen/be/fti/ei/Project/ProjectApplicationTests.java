ea70dbc83e660b9f88f320da927d684b58f91587
wassi <wassimhajjab@hotmail.com>
2025-03-20 11:36:59 +0100

    Initial commit

---
+++ src/test/java/uantwerpen/be/fti/ei/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uantwerpen.be.fti.ei.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
